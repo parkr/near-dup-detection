4bbf92d4b864ee4938b97b1bf3bd15b73f6bc082
Parker Moore <parkrmoore@gmail.com>
2012-12-01 16:07:08 -0500

    Inserting path to project files into detector_spec

--- unit/detector_spec.py
+++ unit/detector_spec.py
@@ -2,7 +2,8 @@
 
 import unittest
 import sys
-sys.path.insert(0, './../')
+from os.path import dirname, abspath
+sys.path.insert(0, dirname(dirname(abspath(__file__))))
 from detector import Detector
 
 class TestSequenceFunctions(unittest.TestCase):
