ec62cb95cf86e18f68219e40c11f68d2eed75c66
Parker Moore <parkrmoore@gmail.com>
2012-12-01 16:30:24 -0500

    Added Ngram.containing_files list

--- ngram.py
+++ ngram.py
@@ -3,7 +3,10 @@ class Ngram:
     def __init__(self, index, value, containing_file):
         self.index = index
         self.value = value
-        self.containing_file = containing_file
+        self.containing_files = [containing_file]
+    
+    def add_containing_file(self, containing_file):
+        self.containing_files.append(containing_file)
     
     def __eq__(self, other):
         return self.value == other.value
