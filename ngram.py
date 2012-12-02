e8d16039cf520a600f531d6e6cd9652d5d183980
Parker Moore <parkrmoore@gmail.com>
2012-12-02 18:52:23 -0500

    Added __str__(), __repr__(), __hash__() and .ID to Ngram

--- ngram.py
+++ ngram.py
@@ -1,15 +1,25 @@
 class Ngram:
     
     def __init__(self, index, value, containing_file):
-        self.index = index
+        self.indx = index
         self.value = value
         self.containing_files = [containing_file]
+        self.ID = self.__hash__()
     
     def add_containing_file(self, containing_file):
         self.containing_files.append(containing_file)
     
     def __eq__(self, other):
-        return self.value == other.value
+        return self.value.__eq__(other.value)
     
     def __hash__(self):
         return self.value.__hash__()
+    
+    def __str__(self):
+        return self.value.__str__()
+    
+    def __repr__(self):
+        return self.value.__str__()
+
+    def __hash__(self):
+        return hash(self.value)
