c6616eb7bbf72d65cb083e5ba2811ac6146b872c
Parker Moore <parkrmoore@gmail.com>
2012-12-02 19:08:15 -0500

    Printing three nearest neighbors of first 10 docs

--- ndd.py
+++ ndd.py
@@ -1,9 +1,40 @@
 #! /usr/bin/env python
 
+import operator, copy
 from detector import Detector
 
 if __name__ == "__main__":
     # run the program
     detector = Detector('./test')
     print "Checking for duplicates using NDD..."
-    print detector.check_for_duplicates()
+    duplicates = detector.check_for_duplicates()
+    if duplicates:
+        print "Duplicates found (Jaccard coefficient > 0.5):"
+        print duplicates
+    
+    print "Printing three nearest neighbors of the first 10 files..."
+    filenames_of_first_ten = [detector.filename("file%02d.txt") % (f,) for f in xrange(10)]
+    for index1, j in enumerate(filenames_of_first_ten):
+        jaccard_coefficients = [0,0,0,0,0,0,0,0,0,0]
+        for index2, d in enumerate(filenames_of_first_ten):
+            if index1 != index2:
+                jaccard_coefficients[index2] = detector.get_jaccard(j, d)
+        three_nearest = []
+        nearest_count = -1
+        jcos = copy.deepcopy(jaccard_coefficients)
+        while len(three_nearest) < 3:
+            index,coefficient = max(enumerate(jcos), key=operator.itemgetter(1))
+            del jcos[index]
+            # put the index back where it was in the original jaccard_coefficients
+            if nearest_count == 0 and index >= three_nearest[0][0]:
+                index += 1
+            if nearest_count == 1:
+                if index >= three_nearest[0][0]:
+                    index += 1
+                if index >= three_nearest[1][0]:
+                    index += 1
+            three_nearest.append((index,coefficient))
+            nearest_count += 1
+        print "Three nearest neighbors to %s:" % ("file%02d.txt" % index1)
+        for near in sorted(three_nearest, key=operator.itemgetter(1), reverse=True):
+            print "\t%s with Jaccard coefficient of %0.3f" % ("file%02d.txt" % near[0], near[1])
